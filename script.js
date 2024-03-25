e3cdc0bcbbede0a8bc4bb940e1777f44f755bc55
Alutaman <164894638+Alutaman@users.noreply.github.com>
2024-03-25 18:46:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// Define a list of possible usernames
-var usernames = ["elvisblatter@gmail.com", "root", "support", "user"];
-
-// Define a target username
-var targetUsername = "elvisblatter@gmail.com";
-
-// Loop through the list of usernames and try to guess the target username
-for (var i = 0; i < usernames.length; i++) {
-    if (usernames[i] === targetUsername) {
-        console.log("Username found: " + targetUsername);
-        break;
-    }
-}
+type="text/javascript">
+document.cookie = "username=Null Byte";
+document.write(document.cookie);
