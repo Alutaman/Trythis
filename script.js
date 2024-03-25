8f55ce4177458e7f3299f6adefe0c251f7f29459
Alutaman <164894638+Alutaman@users.noreply.github.com>
2024-03-25 18:22:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+// Define a list of possible usernames
+var usernames = ["admin", "root", "support", "user"];
+
+// Define a target username
+var targetUsername = "admin";
+
+// Loop through the list of usernames and try to guess the target username
+for (var i = 0; i < usernames.length; i++) {
+    if (usernames[i] === targetUsername) {
+        console.log("Username found: " + targetUsername);
+        break;
+    }
+}
