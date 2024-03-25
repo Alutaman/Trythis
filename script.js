2cae5e40b92cd2ce7b42b852200d1ec3bf8b9895
Alutaman <164894638+Alutaman@users.noreply.github.com>
2024-03-25 18:28:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Define a list of possible usernames
-var usernames = ["admin", "root", "support", "user"];
+var usernames = ["elvisblatter@gmail.com", "root", "support", "user"];
 
 // Define a target username
-var targetUsername = "admin";
+var targetUsername = "elvisblatter@gmail.com";
 
 // Loop through the list of usernames and try to guess the target username
 for (var i = 0; i < usernames.length; i++) {
